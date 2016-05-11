44fa1253e2a3f45509cddaf2dee706ea1895949c
Eli Velichkova <calipsomn@yahoo.com>
2016-05-11 11:49:04 +0300

    Homework 1_1

---
+++ Homework1/01_Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllocateArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int ind = 0; ind < n; ind++)
+                Console.WriteLine(ind * 5);
+        }
+    }
+}
