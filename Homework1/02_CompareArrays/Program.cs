3d0c01b41304dadd43377b098843b7ebbf51adae
Eli Velichkova <calipsomn@yahoo.com>
2016-05-11 15:06:46 +0300

    homework

---
+++ Homework1/02_CompareArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_CompareArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array1 = new int[n];
+            int[] array2 = new int[n];
+            for (int ind = 0; ind < n; ind++)
+                array1[ind] = int.Parse(Console.ReadLine());
+            for (int ind = 0; ind < n; ind++)
+                array2[ind] = int.Parse(Console.ReadLine());
+            bool equal = true;
+            for (int ind = 0; ind < n; ind++)
+                if (array1[ind] != array2[ind])
+                {
+                    equal = false;
+                    break;
+                }
+            Console.WriteLine(equal ? "Equal" : "Not equal");
+        }
+    }
+}
---
+++ Homework1/03_CompareCharArrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_CompareCharArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var charArr1 = Console.ReadLine().ToCharArray();
+            var charArr2 = Console.ReadLine().ToCharArray();
+            int length = charArr1.Length > charArr2.Length ? charArr1.Length : charArr2.Length;
+            for (int ind = 0; ind < length; ind++)
+            {
+                if (charArr1.Length < length)
+                {
+                    Console.WriteLine("<");
+                    return;
+                }
+                if (charArr2.Length < length)
+                {
+                    Console.WriteLine(">");
+                    return;
+                }
+                if (charArr1[ind] > charArr2[ind])
+                {
+                    Console.WriteLine(">");
+                    return;
+                }
+                if (charArr1[ind] < charArr2[ind])
+                {
+                    Console.WriteLine("<");
+                    return;
+                }
+            }
+            Console.WriteLine("=");
+        }
+    }
+}
