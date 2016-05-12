4636f1ca1ec94f50824bf0e720264d65ed6645f3
Eli Velichkova <calipsomn@yahoo.com>
2016-05-12 15:31:07 +0300

    Homework multidimensional arrays

---
+++ MultidimentionalArrays/MaximalSum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstRowNumbers = Console.ReadLine().Split(' ');
+            int n = int.Parse(firstRowNumbers[0]);
+            int m = int.Parse(firstRowNumbers[1]);
+            int[,] matrix = new int[n, m];
+            for (int rowInd = 0; rowInd < n; rowInd++)
+            {
+                var numbers = Console.ReadLine().Split(' ');
+                for (int colInd = 0; colInd < numbers.Count(); colInd++)
+                    matrix[rowInd, colInd] = int.Parse(numbers[colInd]);
+            }
+            int maxSum = int.MinValue;
+            int subNumber = 3;
+            for (int rowInd = 0; n >= subNumber ? rowInd <= n - subNumber : rowInd < n; rowInd++)
+            {
+                for (int colInd = 0; n>= subNumber? colInd <= n - subNumber: colInd < n; colInd++)
+                {
+                    int currentSum = 0;
+                    for (int currentRowInd = rowInd; currentRowInd < rowInd + subNumber && currentRowInd < n; currentRowInd++)
+                    {
+                        for (int currentColInd = colInd; currentColInd < colInd + subNumber && currentColInd < n; currentColInd++)
+                        {
+                            currentSum += matrix[currentRowInd, currentColInd];
+
+                        }
+                    }
+                    if (currentSum > maxSum)
+                        maxSum = currentSum;
+                }
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
---
+++ MultidimentionalArrays/SequenceInMatrix/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstRowNumbers = Console.ReadLine().Split(' ');
+            int n = int.Parse(firstRowNumbers[0]);
+            int m = int.Parse(firstRowNumbers[1]);
+            string[,] matrix = new string[n, m];
+            for (int rowInd = 0; rowInd < n; rowInd++)
+            {
+                var numbers = Console.ReadLine().Split(' ');
+                for (int colInd = 0; colInd < numbers.Count(); colInd++)
+                    matrix[rowInd, colInd] = numbers[colInd];
+            }
+            int maxSequence = 0;
+            for (int rowInd = 0; rowInd < n; rowInd++)
+            {
+                for (int colInd = 0; colInd < n; colInd++)
+                {
+                    var currentString = matrix[rowInd, colInd];
+                    int currentSequence = 0;
+                    for (int currentCol = colInd; currentCol < n; currentCol++)
+                    {
+                        if (matrix[rowInd, currentCol] == currentString)
+                            currentSequence++;
+                        else
+                            break;
+                    }
+                    if (currentSequence > maxSequence)
+                        maxSequence = currentSequence;
+
+                    currentSequence = 0;
+                    for (int currentRow = rowInd; currentRow < n; currentRow++)
+                    {
+                        if (matrix[currentRow, colInd] == currentString)
+                            currentSequence++;
+                        else
+                            break;
+                    }
+                    if (currentSequence > maxSequence)
+                        maxSequence = currentSequence;
+
+                    currentSequence = 0;
+                    for (int currentRow = rowInd, currentCol = colInd; currentRow < n && currentCol < n; currentRow++, currentCol++)
+                    {
+                        if (matrix[currentRow, currentCol] == currentString)
+                            currentSequence++;
+                        else
+                            break;
+                    }
+                    if (currentSequence > maxSequence)
+                        maxSequence = currentSequence;
+
+                    currentSequence = 0;
+                    for (int currentRow = rowInd, currentCol = n - 1; currentRow < n && currentCol >= 0; currentRow++, currentCol--)
+                    {
+                        if (matrix[currentRow, currentCol] == currentString)
+                            currentSequence++;
+                        else
+                            break;
+                    }
+                    if (currentSequence > maxSequence)
+                        maxSequence = currentSequence;
+                }
+            }
+            Console.WriteLine(maxSequence);
+        }
+    }
+}
