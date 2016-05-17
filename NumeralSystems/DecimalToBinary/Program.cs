38c09ddcdeb73be6c1fb87fc7d68e1a3ae74b803
Eli Velichkova <calipsomn@yahoo.com>
2016-05-17 19:06:31 +0300

    Homework

---
+++ NumeralSystems/DecimalToBinary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = ulong.Parse(Console.ReadLine());
+            Console.WriteLine(DecimalToBunary(n));
+        }
+
+        public static string DecimalToBunary(ulong number)
+        {
+            var binary = new StringBuilder();
+            var currentNumber = number;
+            while (currentNumber > 0)
+            {
+                if (currentNumber % 2 == 0)
+                    binary.Insert(0, 0);
+                else
+                    binary.Insert(0, 1);
+                currentNumber /= 2;
+            }
+            return binary.ToString();
+        }
+    }
+}
---
+++ NumeralSystems/HexadecimalToBinary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexadecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetBinaryFromHex(Console.ReadLine()));
+        }
+
+        public static string GetBinaryFromHex(string hexNumber)
+        {
+            StringBuilder binary = new StringBuilder();
+            foreach (char ch in hexNumber.ToLower())
+            {
+                switch (ch)
+                {
+                    case '0': binary.Append("0000"); break;
+                    case '1': binary.Append("0001"); break;
+                    case '2': binary.Append("0010"); break;
+                    case '3': binary.Append("0011"); break;
+                    case '4': binary.Append("0100"); break;
+                    case '5': binary.Append("0101"); break;
+                    case '6': binary.Append("0110"); break;
+                    case '7': binary.Append("0111"); break;
+                    case '8': binary.Append("1000"); break;
+                    case '9': binary.Append("1001"); break;
+                    case 'a': binary.Append("1010"); break;
+                    case 'b': binary.Append("1011"); break;
+                    case 'c': binary.Append("1100"); break;
+                    case 'd': binary.Append("1101"); break;
+                    case 'e': binary.Append("1110"); break;
+                    case 'f': binary.Append("1111"); break;
+                }
+            }
+            return binary.ToString().TrimStart('0');
+        }
+    }
+}
