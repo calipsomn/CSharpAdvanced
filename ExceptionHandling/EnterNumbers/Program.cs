b20997e0855c6db4c966fd243db4ad48c409ae46
Eli Velichkova <calipsomn@yahoo.com>
2016-05-30 16:16:31 +0300

    Homework

---
+++ ExceptionHandling/EnterNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            StringBuilder sb = new StringBuilder("1 < ");
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    int currentNumber = int.Parse(Console.ReadLine());
+                    if (currentNumber <= number)
+                    {
+                        Console.WriteLine("Exception");
+                        return;
+                    }
+                    if (i < 9)
+                        sb.Append(currentNumber + " < ");
+                    else
+                        sb.Append(currentNumber + " < 100");
+                    number = currentNumber;
+                }
+                Console.WriteLine(sb.ToString());
+            }
+            catch
+            {
+                Console.WriteLine("Exception");
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/SquareRoot/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var number = double.Parse(Console.ReadLine());
+                if (number > 0.0)
+                    Console.WriteLine(Math.Sqrt(number).ToString("0.000"));
+                else
+                    Console.WriteLine("Invalid number");
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number");
+            }
+            Console.WriteLine("Good bye");
+        }
+    }
+}
